c5875aa08546ff9b496493441f42cec044883ee9
王辰軒 <chenhsuan@wangchenxuandeMacBook-Pro.local>
2021-01-20 17:22:48 +0800

    2.增加靜態增加靜態html

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Blob</h1>
+<form action="index.blade.php" method="post">
+　<p>文字輸入欄位:</p> <input type="test" name="YourName" value = 'howard'>
+　<input type="submit" value="送出表單">
+</form>
+
+
+</body>
+</html>
